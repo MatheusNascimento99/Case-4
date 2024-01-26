5817fe59f15b8fd3bc20dd37eb531ecdc91b989d
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-01-26 13:02:12 -0300

    inicio lógica estrutural

---
+++ script.js
@@ -0,0 +1,30 @@
+var perguntaDois = window.document.getElementById("pergunta_dois");
+var perguntaTres = window.document.getElementById("pergunta_tres");
+var respostaTresA = window.document.getElementById("resposta_back_rapidez");
+var respostaTresB= window.document.getElementById("resposta_back_prog_int");
+var respostaTresC= window.document.getElementById("resposta_back_facil_int");
+
+
+
+
+function mostrarPerguntaDois (){
+    perguntaDois.style = "display:block"
+}
+
+function mostrarPerguntaTres (){
+    perguntaTres.style = "display:flex"
+}
+
+
+//*RESPOSTAS - BACKEND
+function mostrarRespostaTresA (){
+    respostaTresA.style = "display:block"
+}
+
+function mostrarRespostaTresB (){
+    respostaTresB.style = "display:block"
+}
+
+function mostrarRespostaTresC (){
+    respostaTresC.style = "display:block"
+}
